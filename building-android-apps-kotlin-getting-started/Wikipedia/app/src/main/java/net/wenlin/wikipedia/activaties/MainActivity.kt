94b8bd90d9759ddef058c9dad4cd14daaf3fa9f6
Wenlin Huang <jackhwl@hotmail.com>
2019-08-23 08:59:33 -0400

    Using the Bottom Navitation View with Kotlin

--- building-android-apps-kotlin-getting-started/Wikipedia/app/src/main/java/net/wenlin/wikipedia/activaties/MainActivity.kt
+++ building-android-apps-kotlin-getting-started/Wikipedia/app/src/main/java/net/wenlin/wikipedia/activaties/MainActivity.kt
@@ -7,13 +7,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import net.wenlin.wikipedia.R
+import net.wenlin.wikipedia.fragments.ExploreFragment
+import net.wenlin.wikipedia.fragments.FavoritesFragment
+import net.wenlin.wikipedia.fragments.HistoryFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
+    private val exploreFragment: ExploreFragment
+    private val favoritesFragment: FavoritesFragment
+    private val historyFragment: HistoryFragment
+
+    init {
+        exploreFragment = ExploreFragment()
+        favoritesFragment = FavoritesFragment()
+        historyFragment = HistoryFragment()
+    }
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
 
-        false
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+
+        when(item.itemId) {
+            R.id.navigation_explore -> transaction.replace(R.id.fragment_container, exploreFragment)
+            R.id.navigation_favorites -> transaction.replace(R.id.fragment_container, favoritesFragment)
+            R.id.navigation_history -> transaction.replace(R.id.fragment_container, historyFragment)
+        }
+
+        transaction.commit()
+
+        true
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,5 +48,9 @@ class MainActivity : AppCompatActivity() {
 //
 //        textMessage = findViewById(R.id.message)
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        var transaction = supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_container, exploreFragment)
+        transaction.commit()
     }
 }
