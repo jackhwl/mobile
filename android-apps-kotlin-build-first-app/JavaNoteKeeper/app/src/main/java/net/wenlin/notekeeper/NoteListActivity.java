e937fc67729ea11512117751c6832fdd8bdd12a5
Wenlin Huang <jackhwl@hotmail.com>
2019-09-01 22:38:43 -0400

    Passing the Selected Note Through an Intent

--- android-apps-kotlin-build-first-app/JavaNoteKeeper/app/src/main/java/net/wenlin/notekeeper/NoteListActivity.java
+++ android-apps-kotlin-build-first-app/JavaNoteKeeper/app/src/main/java/net/wenlin/notekeeper/NoteListActivity.java
@@ -40,7 +40,7 @@ public class NoteListActivity extends AppCompatActivity {
     }
 
     private void initializeDisplayContent() {
-        ListView listNotes = findViewById(R.id.list_notes);
+        final ListView listNotes = findViewById(R.id.list_notes);
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
         ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
@@ -51,6 +51,8 @@ public class NoteListActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
                 Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+                intent.putExtra(NoteActivity.NOTE_INFO, note);
                 startActivity(intent);
             }
         });
