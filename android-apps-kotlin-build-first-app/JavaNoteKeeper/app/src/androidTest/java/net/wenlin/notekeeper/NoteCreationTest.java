974af97441749d309b840ffd8913625c13ff7654
Wenlin Huang <jackhwl@hotmail.com>
2019-09-07 20:12:38 -0400

    Implementing Basic Test Interactions

---
+++ android-apps-kotlin-build-first-app/JavaNoteKeeper/app/src/androidTest/java/net/wenlin/notekeeper/NoteCreationTest.java
@@ -0,0 +1,31 @@
+package net.wenlin.notekeeper;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class NoteCreationTest {
+    @Rule
+    public ActivityTestRule<NoteListActivity> mNoteListActivityRule = new ActivityTestRule<>(NoteListActivity.class);
+
+    @Test
+    public void createNwNote() {
+//        ViewInteraction fabNewNote = onView(withId(R.id.fab));
+//        fabNewNote.perform(click());
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.text_note_title)).perform(typeText("Test note title"));
+        onView(withId(R.id.text_note_text)).perform(typeText("This is the body of our test note"), closeSoftKeyboard());
+    }
+}
