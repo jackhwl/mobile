9415e658cc8309db0326aa7c9e11c096937d7818
Wenlin Huang <jackhwl@hotmail.com>
2019-09-10 08:01:53 -0400

    implementing RecycleView Adapter

---
+++ android-apps-kotlin-build-first-app/JavaNoteKeeper/app/src/main/java/net/wenlin/notekeeper/NoteRecycleAdapter.java
@@ -0,0 +1,49 @@
+package net.wenlin.notekeeper;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteRecycleAdapter extends RecyclerView.Adapter<NoteRecycleAdapter.ViewHolder> {
+    private final Context mContext;
+    private final LayoutInflater mLayoutInflater;
+
+    public NoteRecycleAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(mContext);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mLayoutInflater.inflate(R.layout.item_note_list, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public final TextView mTextCourse;
+        public final TextView mTextTitle;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mTextCourse = (TextView) itemView.findViewById(R.id.text_course);
+            mTextTitle = (TextView) itemView.findViewById(R.id.text_title);
+        }
+    }
+}
